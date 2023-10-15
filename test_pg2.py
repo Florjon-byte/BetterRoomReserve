f872b20ad323edc28ad601bfc9a7657c4f3b97a1
DarrienGopaul <dgopaul9217@gmail.com>
2023-10-15 02:57:38 -0400

    fixed pgsql test file

--- test_pg2.py
+++ test_pg2.py
@@ -1,13 +1,22 @@
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 # Connect to your postgres DB
-conn = psycopg2.connect("dbname=test user=postgres")
+conn = psycopg2.connect("dbname=betterroomreserve user=postgres")
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
-# Execute a query
-cur.execute("SELECT * FROM my_data")
+# create brr database
+cur.execute("create database BetterRoomReserve;")
 
-# Retrieve query results
-records = cur.fetchall()
+# create a user_info table
+cur.execute("create table user_info (netId varchar(256), verification varchar(256), sso bool);")
+conn.commit()
+
+# sample get request to prove we did the work
+cur.execute("select * from user_info")
+
+# print your information
+print(cur.fetchall())
