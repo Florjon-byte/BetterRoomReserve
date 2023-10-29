84e7e57b1a38c48120a39ec89f1cfa1f05865d3a
florjon-byte <fh999@nyu.edu>
2023-10-29 03:36:26 -0400

    setting up more homepage

--- better-room-reserve-app/src/App.js
+++ better-room-reserve-app/src/App.js
@@ -53,38 +53,38 @@ class App extends Component {
 
         {/* Second Div */}
         <div className='second'>
-          <div className='purposetext'>
+          <div className='purposetexttwo'>
             <h1 className='header'>Library Information and Policies</h1>
             <p> 
-              Amidst the tranquil rustling of leaves in the ancient forest, a solitary fox ventured cautiously along a winding woodland path. The dappled sunlight filtered through the dense canopy, casting intricate patterns of light and shadow upon the moss-covered ground. With a delicate grace, the fox paused, its keen senses alert to the forest's secrets. Nature's symphony played on, as birdsong blended with the distant babbling of a hidden brook, creating a mesmerizing, harmonious tapestry of sounds. In this enchanted realm, time seemed to stand still, and the fox, a silent sentinel, moved with the rhythm of the woods, a fleeting embodiment of the wild's enduring beauty.
+            Group study rooms are a popular service. To help this work well for all students, please adhere to the following policies:
             </p>
+            <ul></ul>
+              <li> 
+                Do not eat full meals or messy foods in the study rooms. If you don’t comply, you will be asked to leave.
+              </li>
+              <li>
+                Rooms cannot be reserved or held by placing personal belongings in them. Materials and personal items left unattended may be removed by library staff.
+              </li>
+              <li>
+                Group rooms may not be reserved or held for a single user only. Single users in a group study room (with or without reservations) will be asked to vacate the room for groups of 2 or more for small rooms, and 4 or more for large rooms.
+              </li>
+              <li>
+                For small group study rooms, we ask that least 2 students occupy the room. For large group study rooms, we ask that at least 4 students occupy the room.
+              </li>
+              <li>
+                Have your email confirmation handy as you may need to verify your reservation.
+              </li>
+              <li>
+              There is a 15 minute grace period for reservations. Reservations will be canceled if a group is more than 15 minutes late.
+              </li>
           </div>
         </div>
-
         {/* Footer */}
+        <div> 
+        </div>
       </div>
     );
   }
 }
 
 export default App;
-
-
-// <div class="row">
-//           <div className="col dibner-image ">
-//             {/* Left Side  */}
-//             <div class="leftside">
-//               <img class="img-fluid rounded float-start" src={logo}></img>
-//               {/* <img className="front img-fluid rounded" src={front}></img> */}
-//             </div>
-//           </div>
-//           <div className="col">
-//             {/* Right Side  */}
-//             <div class="rightside">
-//               <div class="d-grid gap-2 col-6 mx-auto">
-//                 <button class="btn btn-primary" type="button">Button</button>
-//                 <button class="btn btn-primary" type="button">Button</button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
