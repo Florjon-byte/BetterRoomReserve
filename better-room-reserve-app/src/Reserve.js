100597a4f41024ed2a98f7d7e566118bc15bbb1e
florjon-byte <fh999@nyu.edu>
2023-11-12 16:58:04 -0500

    before merge

---
+++ better-room-reserve-app/src/Reserve.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React, { Component, useEffect, useState, Route } from "react"
+import { useNavigate } from 'react-router-dom'
+import axios from "axios"
+
+class Reserve extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        <div>
+            Hello world 
+        </div>
+    }
+}
